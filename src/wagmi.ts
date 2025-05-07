a03c2764c4ed12f6a912ab44d9a9b66f2e0e6c64
lxc <xli503441@gmail.com>
2025-05-08 02:04:57 +0800

    Add sepolia network

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -9,9 +9,13 @@ import {
 } from 'viem/chains';
 import { http } from 'viem';
 
+const alchemyApiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!;
+const walletProjectId = process.env.NEXT_PUBLIC_WALLET_PROJECT_ID!;
+const sepoliaUrl = `https://eth-sepolia.g.alchemy.com/v2/${alchemyApiKey}`;
+
 export const config = getDefaultConfig({
-  appName: 'RainbowKit demo',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: 'Wallet_web3',
+  projectId: walletProjectId,
   chains: [
     mainnet,
     polygon,
@@ -27,6 +31,6 @@ export const config = getDefaultConfig({
     [optimism.id]: http(),
     [arbitrum.id]: http(),
     [base.id]: http(),
-    [sepolia.id]: http(),
+    [sepolia.id]: http(sepoliaUrl),
   },
 });
